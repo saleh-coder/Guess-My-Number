f1a1e89a7da99fb47d1d6a22ad86cf234d226fb7
saleh-coder <amalsalehfernandes@gmail.com>
2024-09-12 09:41:09 -0300

    Implementing the game logic

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
